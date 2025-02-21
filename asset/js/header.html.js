877ab06e4b619af90da18a8e11855e79fb4dfc53
hiwang <hiwang@tencent.com>
2025-02-22 03:12:10 +0800

    first commit

--- asset/js/header.html.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    let activeMenu = location.pathname.split('/').pop()
-    activeMenu = activeMenu.replace('.', '\\.')
-    if (activeMenu.length > 0 && $('#' + activeMenu).length) {
-        $('#' + activeMenu).addClass('blog-nav-item-active')
-    } else {
-        $('#\\/').addClass('blog-nav-item-active')
-    }
-});
